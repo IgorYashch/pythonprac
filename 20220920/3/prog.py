d4ce4e11ea53a8c1613efa8aeeba6289b9ba94a6
IgorYashch <yashch.igor@gmail.com>
2022-09-20 19:08:36 +0300

    Add third task with tests for 20220920

---
+++ 20220920/3/prog.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+def digits_sum(x):
+	s = 0
+	while x:
+		x, digit = divmod(x, 10)
+		s += digit
+	return s
+
+
+k = 0
+while k < 3:
+	l = 0
+	while l < 2:
+		mul = (n + k) * (n + l)
+		print('{} * {} = {}'.format(n + k, n + l, mul if digits_sum(mul) != 6 else ':=)'), end = ' ')
+		l += 1
+	else:
+		mul = (n + k) * (n + l)
+		print('{} * {} = {}'.format(n + k, n + l, mul if digits_sum(mul) != 6 else ':=)'))
+	k += 1
