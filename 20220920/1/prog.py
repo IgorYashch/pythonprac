5dacc931638b9b6bd904798bee15f71e8601b807
IgorYashch <yashch.igor@gmail.com>
2022-09-25 22:27:27 +0300

    Reformat prog.py

--- 20220920/1/prog.py
+++ 20220920/1/prog.py
@@ -1,12 +1,14 @@
 x = int(input())
 
 checker = ['-'] * 3
+
 if x % 25 == 0:
-  if x % 2 == 0:
-    checker[0] = '+'
-  else:
-    checker[1] = '+'
+    if x % 2 == 0:
+        checker[0] = '+'
+    else:
+        checker[1] = '+'
+
 if x % 8 == 0:
-  checker[2] = '+'
+    checker[2] = '+'
 
 print("A {} B {} C {}".format(*checker))
