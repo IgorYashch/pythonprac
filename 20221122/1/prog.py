b431324cccd7eea67457444f913aeab0bebd9dcb
IgorYashch <yashch.igor@gmail.com>
2022-11-26 00:36:18 +0300

    Add task#3 for 20221122

---
+++ 20221122/1/prog.py
@@ -0,0 +1,16 @@
+import sys
+
+s = sys.stdin.buffer.read()
+n = int(s[0])
+blocks = []
+
+curr_pos = 1
+
+
+
+for i in range(1, n + 1):
+    size = int(i * (len(s) - curr_pos) / n)
+    blocks.append(s[curr_pos:curr_pos + size])
+    curr_pos += size
+
+print(s[:1] + b''.join(sorted(blocks)))
