d98cbbb4eea1a4677052cc6a2576f8af2a980009
IgorYashch <yashch.igor@gmail.com>
2022-12-05 14:01:01 +0300

    Add task#1 for 20221129

--- 20221129/1/prog.py
+++ 20221129/1/prog.py
@@ -1,1 +1,20 @@
-# чтобы сделать коммит
+class dump(type):
+
+    def __print_wrapper(fun, name):
+        def wrapper(self, *args, **kwargs):
+            print(name, ': ', args,', ' , kwargs, sep='')
+            return fun(self, *args, **kwargs)
+        return wrapper
+
+    @staticmethod
+    def __new__(metacls, name, parents, ns, **kwds):
+        cls = super().__new__(metacls, name, parents, ns, **kwds)
+        for name in cls.__dict__:
+            obj = getattr(cls, name)
+            if callable(obj):
+                setattr(cls, name, dump.__print_wrapper(obj, name))
+        return cls
+
+
+import sys
+exec(sys.stdin.read())
