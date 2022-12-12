4fda86bb2c41873dbd088ac4b5ef214a2604ce9e
IgorYashch <yashch.igor@gmail.com>
2022-12-12 20:28:29 +0300

    Add task#1 for 20221206

---
+++ 20221206/1/prog.py
@@ -0,0 +1,30 @@
+import asyncio
+
+evnt = asyncio.Event()
+
+async def writer(queue, time_delay):
+    i = 0
+    while not evnt.is_set():
+        await asyncio.sleep(time_delay)
+        await queue.put(f"{i}_задержка")
+        i += 1
+
+    return
+
+
+async def stacker(queue, stack):
+    while not evnt.is_set():
+        elem = async queue.get()
+        stack.append(elem)
+
+    return stack
+
+
+async def reader(stack, num, time_delay):
+    for i in range(num):
+        await asyncio.sleep(time_delay)
+        elem = stack.pop()
+        print(elem)
+
+    evnt.set()
+    return
