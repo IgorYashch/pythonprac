211d4c2865a091b41e86f3cd6d1e4d4d3bc2a611
IgorYashch <yashch.igor@gmail.com>
2023-02-20 02:08:11 +0300

    20230213 prog.py PART3

--- 20230213/prog.py
+++ 20230213/prog.py
@@ -37,6 +37,42 @@ def get_commit_body(repo_path, commit_id):
     return body
 
 
+def decode_tree(path):
+    result = []
+    with open(path, "rb") as f:
+        file = zlib.decompress(f.read())
+        header, _, tail = file.partition(b"\x00")
+
+    while tail:
+        treeobj, _, tail = tail.partition(b"\x00")
+        tmode, tname = treeobj.split()
+        num, tail = tail[:20], tail[20:]
+        if tmode.decode() == "40000":
+            tmode_name = "tree"
+        elif tmode.decode() == "100644":
+            tmode_name = "blob"
+
+        result.append((tmode_name, num.hex(), tname.decode()))
+
+    return result
+
+
+# Task 3
+def get_tree_for_last_commit(repo_path, commit_id):
+    paths = os.path.join(repo_path, OBJECTS_PATHS)
+
+    commit_body = get_commit_body(repo_path, commit_id)
+    tree_id = commit_body.split()[1]
+
+    for store in glob.iglob(paths):
+        id = basename(dirname(store)) + basename(store)
+        if id == tree_id:
+            tree_body = decode_tree(store)
+            break
+
+    return tree_body
+
+
 def main():
     branches_paths = get_branches(REPO_PATH)
     branches_names = [basename(b) for b in branches_paths]
@@ -52,8 +88,13 @@ def main():
         last_commit_id = open(branch_path, "rb").read().rstrip().decode()
 
         # # Task 2
-        commit_body = get_commit_body(REPO_PATH, last_commit_id)
-        print(commit_body)
+        # commit_body = get_commit_body(REPO_PATH, last_commit_id)
+        # print(commit_body)
+
+        # Task 3
+        tree_body = get_tree_for_last_commit(REPO_PATH, last_commit_id)
+        for tmode, id, name in tree_body:
+            print(f'{tmode} {id}    {name}')
 
 
 if __name__ == "__main__":
