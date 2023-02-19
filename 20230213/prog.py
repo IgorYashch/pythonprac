10e2ce6dfc49f82d79c9ea9e449e9c7e2de00c7f
IgorYashch <yashch.igor@gmail.com>
2023-02-20 00:03:10 +0300

    Выводит объект дерево на который указывает последний коммит (пункт 3)

--- 20230213/prog.py
+++ 20230213/prog.py
@@ -19,9 +19,44 @@ def print_commit(path, commit_id):
             header, _, body = obj.partition(b'\x00')
             kind, size = header.split()
 
-        if kind == b'commit' and id == commit_id:
+        if id == commit_id:
             out = body.decode()
             print(f"{out}")
+
+
+def print_last_tree(path, commit_id):
+    for store in glob.iglob(path):
+        id = basename(dirname(store)) + basename(store)
+
+        with open(store, "rb") as f:
+            obj = zlib.decompress(f.read())
+            header, _, body = obj.partition(b'\x00')
+            kind, size = header.split()
+
+        if kind == b'commit' and id == commit_id:
+            tree_id = body.decode().split()[1]
+            break
+
+    for store in glob.iglob(path):
+        id = basename(dirname(store)) + basename(store)
+        if id == tree_id:
+            with open(store, "rb") as f:
+                obj = zlib.decompress(f.read())
+                header, _, body = obj.partition(b'\x00')
+                kind, size = header.split()
+
+            tail = body
+            while tail:
+                treeobj, _, tail = tail.partition(b'\x00')
+                tmode, tname = treeobj.split()
+                num, tail = tail[:20], tail[20:]
+                if tmode.decode() == '40000':
+                    tmode_name = "tree"
+                elif tmode.decode() == "100644":
+                    tmode_name = "blob"
+
+                print(f"{tmode_name} {num.hex()}    {tname.decode()}")
+            break
 
 branches = get_branches()
 
@@ -37,5 +72,5 @@ else:
     my_branch_commit_id = open(branch_path, "rb").read().rstrip().decode()
     # print(my_branch_commit_id)
 
-    print_commit("../.git/objects/??/*", my_branch_commit_id)
-
+    # print_commit("../.git/objects/??/*", my_branch_commit_id)
+    print_last_tree("../.git/objects/??/*", my_branch_commit_id)
